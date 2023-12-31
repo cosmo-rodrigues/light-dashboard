dd7a7732c0648c79ba571c03771aee2905c584f9
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2023-12-31 05:43:04 -0300

    fix(modules) - Import modules resolver

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-import { SideNav } from './SideNav/SideNav';
-import { NavBar } from './NavBar/NavBar';
-import { PastBills } from './PastBills/PastBills';
+import { SideNav } from './SideNav/SideNav.tsx';
+import { NavBar } from './NavBar/NavBar.tsx';
+import { PastBills } from './PastBills/PastBills.tsx';
 
 import * as Charts from './Charts';
 import * as Fatura from './Fatura';
